fa2fe9be0cc94daf65c5413c8c1ab815ae47efd1
Rodrigo  Vidal Guzman Diaz <rvguzman@scl.edu.gt>
2022-07-23 19:54:05 -0600

    Se completo la base del trabajo de grupo

---
+++ js/category.js
@@ -0,0 +1,11 @@
+let category =[
+    /*ID_de_usuario*/ /*numero_tarjeta_de_credito*/
+    [4000,          'Laptops'],
+    [4100,          'Mobile phones'],
+    [4200,          'Tablets'],
+    [4300,          'Servers'],
+    [4400,          'PC'],
+    [4500,          'Cables'],
+    [4600,          'Headphones'],
+
+];
---
+++ js/login.js
@@ -0,0 +1,39 @@
+/** LIBRERIAS**/
+export var section_administrador = document.getElementById("section_administrador");
+export var section_login = document.getElementById("section_login");
+
+export var inputPassword = document.getElementById("password")
+export var botonVer = document.getElementById("botonVer");
+botonVer.addEventListener('click', verContraseña)
+
+export function verContraseña(){
+    if(inputPassword.type == "password"){
+        inputPassword.type = "text"
+        botonVer.src = "https://tinyurl.com/2p8pm2yx";
+    }else{
+        inputPassword.type = "password"
+        botonVer.src = "https://tinyurl.com/2dzcdfec";
+    }
+}
+
+// Librería de usuario
+import {user} from "./username.js";
+import { adminstrador } from "./administrador.js";
+
+// Función de verificación de credenciales
+export function login(username,password){
+
+    for(let i = 0; i<=7;i++){
+        if(username === user[i][1] || username === user[i][5]){
+            if(password === user[i][2]){
+                console.log("Ahora tienes acceso");
+                adminstrador();
+            }else{
+                console.log('Constraseña incorrecta');
+            }
+        }else {
+            console.log("Usuario incorrecto");
+        }
+    }
+
+}
